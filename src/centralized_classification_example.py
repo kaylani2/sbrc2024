d00eb5db35fd762e8b1689b62ea6a7abe8bd150a
Kaylani <kaylani@gta.ufrj.br>
2023-10-31 18:39:38 -0300

    Finish centralized poc

--- src/centralized_classification_example.py
+++ src/centralized_classification_example.py
@@ -1,45 +1,35 @@
 import flwr as fl
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
+from keras.utils import plot_model
+from keras.models import load_model
 
+### Load data
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-print (x_train.shape, y_train.shape)
+x_train = x_train [0:512]
+y_train = y_train [0:512]
 
-#count=1
-#idd=0
-#plt.figure(figsize = (10,10))
-#for i in range (5):
-#  for j in range (2):
-#    plt.subplot (5,5,count)
-#    plt.imshow(x_train[idd], cmap='gray')
-#    plt.axis('off')
-#    plt.title('Classe: '+str(y_train[idd]))
-#    idd+=1
-#    count+=1
-#plt.show()
+### Filter data
+train_mask_0_1 = np.isin(y_train, [0, 1])
+x_train, y_train = x_train [train_mask_0_1], y_train [train_mask_0_1]
 
-model = tf.keras.applications.MobileNetV2((32, 32, 1), classes=10, weights=None)
-model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+### Resize data
+x_train = np.expand_dims(x_train, axis=-1)
+x_train = tf.image.resize(x_train, [32,32])
 
 
-history = model.fit (x_train, y_train, epochs=25, batch_size=128, validation_split=0.2, verbose=1)
+### Load/create, train and save model
+try:
+  model = load_model ('mobilenetv2_mnist.keras')
+except:
+  model = tf.keras.applications.MobileNetV2((32,32,1), classes=10, weights=None)
+model.compile ("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+history = model.fit (x_train, y_train, epochs=25, batch_size=64, validation_split=0.2, verbose=2, workers=1, use_multiprocessing=True)
+model.save('mobilenetv2_mnist.keras')
 
 
-exit()
-
-
-class CifarClient(fl.client.NumPyClient):
-    def get_parameters(self, config):
-        return model.get_weights()
-
-    def fit(self, parameters, config):
-        model.set_weights(parameters)
-        model.fit(x_train, y_train, epochs=1, batch_size=32, steps_per_epoch=3)
-        return model.get_weights(), len(x_train), {}
-
-    def evaluate(self, parameters, config):
-        model.set_weights(parameters)
-        loss, accuracy = model.evaluate(x_test, y_test)
-        return loss, len(x_test), {"accuracy": float(accuracy)}
-
-fl.client.start_numpy_client(server_address="[::]:8080", client=CifarClient())
+### Evaluate model
+x_test = np.expand_dims(x_test, axis=-1)
+x_test = tf.image.resize(x_test, [32,32])
+model.evaluate (x_test, y_test, verbose=2)
