74c2886cb25c2d10e0affe5b7843bdb67ef641e2
Kaylani <kaylani@gta.ufrj.br>
2023-11-25 01:16:52 -0300

    Not in the mood

--- src/fedavg-noniid/plot_accuracies_server.py
+++ src/fedavg-noniid/plot_accuracies_server.py
@@ -5,19 +5,13 @@ plt.rcParams.update({'font.size': 16})
 plt.rc('xtick', labelsize=16)
 plt.rc('ytick', labelsize=16)
 
-
-
-
-
 logfiles = [
-  'server_main_500rounds_2clients_fedavg.log',
-  'server_main_500rounds_5clients_fedavg.log',
-  'server_main_500rounds_10clients_fedavg.log',
-  'server_main_500rounds_15clients_fedavg.log',
-  'server_main_500rounds_25clients_fedavg.log',
-  '1e-4_server_main_500rounds_25clients_fedavg.log',
-  'server_main_500rounds_50clients_fedavg.log',
-  '256bc_server_main_500rounds_50clients_fedavg.log',
+  'logs_plot/server_main_500rounds_2clients_fedavg.log',
+  'logs_plot/server_main_500rounds_5clients_fedavg.log',
+  'logs_plot/server_main_500rounds_10clients_fedavg.log',
+  'logs_plot/server_main_500rounds_15clients_fedavg.log',
+  'logs_plot/server_main_500rounds_25clients_fedavg.log',
+  'logs_plot/server_main_500rounds_50clients_fedavg.log',
 ]
 labels = [
   '2 clientes',
@@ -25,9 +19,7 @@ labels = [
   '10 clientes',
   '15 clientes',
   '25 clientes',
-  '25 clientes 1e-4 LR',
   '50 clientes',
-  '50 clientes 256 B_c',
 ]
 
 for logfile, label in zip(logfiles, labels):
@@ -45,7 +37,7 @@ for logfile, label in zip(logfiles, labels):
   accuracy_list = [float(value) for value in accuracy_values]
 
   x = list(range(1, len(accuracy_list)+ 1))
-  plt.plot(x, accuracy_list, label=label, marker='', alpha=0.5)
+  plt.plot(x, accuracy_list, label=label, marker='', alpha=0.8)
 
 plt.xlabel("Rodada")
 plt.ylabel("Acurácia")
@@ -54,6 +46,6 @@ plt.legend (loc='lower right', ncol=1, frameon=False, markerfirst=True, labelcol
 plt.xlim (0, 501)
 plt.gcf().set_size_inches(12, 6)  # Adjust the figure size (width, height) to fit the legend
 plt.tight_layout()
-plt.show()
-#plt.savefig ('server_accuracy_fedavg_100rounds.pdf')
-#print ('saved')
+#plt.show()
+plt.savefig ('server_accuracy_fedavg-noniid_500rounds_mobilenetv2.pdf')
+print ('saved')
