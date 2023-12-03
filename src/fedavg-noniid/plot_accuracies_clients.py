aa2d94ce271e4de711a3e01c9822d9d5dcc45f08
Kaylani <kaylani@gta.ufrj.br>
2023-12-03 14:58:11 -0300

    Remove negative and >1 accuracy

--- src/fedavg-noniid/plot_accuracies_clients.py
+++ src/fedavg-noniid/plot_accuracies_clients.py
@@ -19,14 +19,14 @@ logfiles_50clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds
 
 ## plot mobilenetv2
 
-#filename = 'client_accuracy_fedavg-noniid_500rounds_mobilenetv2.pdf'
-#logfiles_2clients  = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_02clients_fedavg-noniid/client_main_{i}_2_clients.log" for i in range(1, 2+1)]
-#logfiles_5clients  = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_05clients_fedavg-noniid/client_main_{i}_5_clients.log" for i in range(1, 5+1)]
-#logfiles_10clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_10clients_fedavg-noniid/client_main_{i:02d}_10_clients.log" for i in range(1, 10+1)]
-#logfiles_15clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_15clients_fedavg-noniid/client_main_{i:02d}_15_clients.log" for i in range(1, 15+1)]
-#logfiles_25clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_25clients_fedavg-noniid/client_main_{i:02d}_25_clients.log" for i in range(1, 25+1)]
-#logfiles_50clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_50clients_fedavg-noniid/client_main_{i:02d}_50_clients.log" for i in range(1, 50+1)]
-#
+filename = 'client_accuracy_fedavg-noniid_500rounds_mobilenetv2.pdf'
+logfiles_2clients  = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_02clients_fedavg-noniid/client_main_{i}_2_clients.log" for i in range(1, 2+1)]
+logfiles_5clients  = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_05clients_fedavg-noniid/client_main_{i}_5_clients.log" for i in range(1, 5+1)]
+logfiles_10clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_10clients_fedavg-noniid/client_main_{i:02d}_10_clients.log" for i in range(1, 10+1)]
+logfiles_15clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_15clients_fedavg-noniid/client_main_{i:02d}_15_clients.log" for i in range(1, 15+1)]
+logfiles_25clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_25clients_fedavg-noniid/client_main_{i:02d}_25_clients.log" for i in range(1, 25+1)]
+logfiles_50clients = [f"/mnt/Shared_Folder_With_VM/logs/Non_IID_FedAVG_500rounds_mobilenetv2_not_regularized/logs_500rounds_50clients_fedavg-noniid/client_main_{i:02d}_50_clients.log" for i in range(1, 50+1)]
+
 
 configs = [
   {'label1': 'Média 2 clientes', 'label2': 'Desvio padrão 2 clientes', 'color': 'cyan',},
@@ -60,15 +60,25 @@ for log_group, config in zip (log_groups, configs):
   # Calculate mean and standard deviation across the lists
   mean_values = np.mean(accuracies, axis=0)
   std_dev = np.std(accuracies, axis=0)
+
+  # Adjust mean and std deviation for plotting
+  upper_bound = mean_values + std_dev
+  lower_bound = mean_values - std_dev
+
+  # Replace incorrect negative error bars
+  upper_bound = np.where(upper_bound > 1, 1, upper_bound)
+  lower_bound = np.where(lower_bound < 0, 0, lower_bound)
+
   # Plot
   plt.plot(np.arange(len(mean_values)), mean_values, label=config['label1'], color=config['color'])
-  plt.fill_between(np.arange(len(mean_values)), mean_values - std_dev, mean_values + std_dev, color=config['color'], alpha=0.2, label=config['label2'])
+  plt.fill_between(np.arange(len(mean_values)), lower_bound, upper_bound, color=config['color'], alpha=0.2, label=config['label2'])
+
 
 
 plt.xlabel("Rodada", fontsize=20)
 plt.ylabel("Acurácia", fontsize=20)
 
-plt.legend (loc='lower right', bbox_to_anchor=(0,0,1,1), ncol=2, frameon=False, markerfirst=True, labelcolor='black')
+plt.legend (loc='lower right', bbox_to_anchor=(0,0.1,1,1), ncol=1, frameon=False, markerfirst=True, labelcolor='black')
 
 plt.gcf().set_size_inches(12, 6)
 plt.xlim (0, 501)
